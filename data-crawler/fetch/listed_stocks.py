f99ec6937d78240e8189b6bcb932f4f1d240d0b0
이경우 <kyungwoo@igyeong-uui-MacBookPro-2.local>
2025-07-03 23:44:53 +0900

    list of stocks info request is completed

--- data-crawler/fetch/listed_stocks.py
+++ data-crawler/fetch/listed_stocks.py
@@ -8,28 +8,18 @@ def fetch_listed_stocks(base_date):
     GET 요청으로 상장 종목 리스트를 조회하고, JSON 결과를 출력 및 반환합니다.
     """
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd",
-        "Accept": "application/json, text/javascript, */*; q=0.01"
+        "AUTH_KEY": KRX_API_KEY,
     }
-    data = {
-        "apiid": "stk_isu_base_info",
-        "authKey": KRX_API_KEY,
-        "reqType": "json",
-        "baseDate": base_date
+    params = {
+        "basDd": base_date,
     }
     try:
-        # POST 요청 수행
-        response = requests.post(LISTED_STOCKS_URL, data=data, headers=headers)
+        # GET 요청
+        response = requests.get(LISTED_STOCKS_URL, params=params ,headers=headers)
         response.raise_for_status()  # HTTP 에러 발생 시 예외 발생
         data = response.json()      # JSON 파싱
-        print(data)                 # 결과 JSON 출력
+        print(f"✅ 상장 종목 조회 성공!")
         return data
     except requests.exceptions.RequestException as e:
         print(f"❌ 상장 종목 조회 실패: {e}")
         return None
-
-
-if __name__ == "__main__":
-    # 예시: apiid="123", auth_key="ABCDEF", req_type="01", base_date="20250701"
-    fetch_listed_stocks("123", "ABCDEF", "01", "20250701")
--- data-crawler/main.py
+++ data-crawler/main.py
@@ -1,7 +1,13 @@
+import json
 import db
 from fetch.listed_stocks import fetch_listed_stocks
 
 if __name__ == "__main__":
     # 상장 종목 리스트 조회
-    stocks = fetch_listed_stocks("20250702")
-    print("Fetched stocks:", stocks)
+    base_date = "20250702"
+    stocks = fetch_listed_stocks(base_date)
+    if stocks is not None:
+        filename = f"stocks_{base_date}.json"
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(stocks, f, ensure_ascii=False, indent=2)
+        print(f"Saved JSON data to {filename}")
