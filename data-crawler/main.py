03f76ed3ae84d2d65aad05f02d30deaf631f77b7
이경우 <kyungwoo5826@gmail.com>
2025-07-03 23:53:23 +0900

    git test

--- data-crawler/main.py
+++ data-crawler/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     # 상장 종목 리스트 조회
     base_date = "20250702"
     stocks = fetch_listed_stocks(base_date)
-    if stocks is not None:
-        filename = f"stocks_{base_date}.json"
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dump(stocks, f, ensure_ascii=False, indent=2)
-        print(f"Saved JSON data to {filename}")
+    # if stocks is not None:
+        # filename = f"stocks_{base_date}.json"
+        # with open(filename, "w", encoding="utf-8") as f:
+        #     json.dump(stocks, f, ensure_ascii=False, indent=2)
+        # print(f"Saved JSON data to {filename}")
